c64b9200ef79ab55ea7e4322f1f7c1501ff333fc
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2016-07-27 20:42:42 -0700

    Initial Commit

---
+++ Virtual Tourist/PinLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PinLocationViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ramiro H. Lopez on 7/27/16.
+//  Copyright © 2016 Ramiro H. Lopez. All rights reserved.
+//
+
+import UIKit
+
+class PinLocationViewController: UIViewController {
+    
+}
