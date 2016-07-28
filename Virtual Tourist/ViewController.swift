563f2281333b9aaee37a4f2327264bd32e8e57e0
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2016-07-27 20:33:20 -0700

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ramiro H. Lopez on 7/27/16.
+//  Copyright © 2016 Ramiro H. Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
